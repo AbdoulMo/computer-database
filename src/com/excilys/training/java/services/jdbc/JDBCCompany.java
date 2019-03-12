ae3bb129e96dd6487e81b2481b6daa4f023d12ab
Abdoul Mohamed <amohamed@excilys.com>
2019-03-12 14:37:56 +0100

    partial implementation of JDBC Computer

--- src/com/excilys/training/java/services/jdbc/JDBCCompany.java
+++ src/com/excilys/training/java/services/jdbc/JDBCCompany.java
@@ -1,6 +1,39 @@
 package com.excilys.training.java.services.jdbc;
 
-public class JDBCCompany {
+import java.util.List;
 
-	
+import com.excilys.training.java.modele.Company;
+import com.excilys.training.java.services.interfaces.IDAOCompany;
+
+public class JDBCCompany implements IDAOCompany{
+
+	@Override
+	public Company getCompany(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Company> getAllCompany() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean addCompany(Company c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteCompany(Company c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateCompany(Company c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
--- src/com/excilys/training/java/services/jdbc/JDBCComputer.java
+++ src/com/excilys/training/java/services/jdbc/JDBCComputer.java
@@ -1,6 +1,95 @@
 package com.excilys.training.java.services.jdbc;
 
-public class JDBCComputer {
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-	
+import com.excilys.training.java.modele.Computer;
+import com.excilys.training.java.services.interfaces.IDAOComputer;
+
+public class JDBCComputer implements IDAOComputer {
+
+	private String query;
+	private PreparedStatement preparedStatement;
+	private ResultSet resultSet;
+
+	@Override
+	public Computer getComputer(String name) {
+		Computer c = new Computer();
+		query = "SELECT * FROM computer WHERE name = ?";
+		try {
+			preparedStatement = JDBCConnection.connection().prepareStatement(query);
+			preparedStatement.setString(1, name);
+			resultSet = preparedStatement.executeQuery();
+			if (resultSet.next()) {
+				c.setId(resultSet.getInt("id"));
+				c.setName(resultSet.getString("name"));
+				c.setIntroduced(resultSet.getDate("introduced"));
+				c.setDiscontinued(resultSet.getDate("discontinued"));
+				c.setManufacturer_id(resultSet.getInt("manufacturer_id"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			JDBCConnection.disconnection();
+		}
+		return c;
+	}
+
+	@Override
+	public List<Computer> getAllComputers() {
+		List<Computer> cList = new ArrayList<>();
+		Computer c = new Computer();
+		query = "SELECT * FROM computer";
+		try {
+			preparedStatement = JDBCConnection.connection().prepareStatement(query);
+			resultSet = preparedStatement.executeQuery();
+			if (resultSet.next()) {
+				c.setId(resultSet.getInt("id"));
+				c.setName(resultSet.getString("name"));
+				c.setIntroduced(resultSet.getDate("introduced"));
+				c.setDiscontinued(resultSet.getDate("discontinued"));
+				c.setManufacturer_id(resultSet.getInt("manufacturer_id"));
+				cList.add(c);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			JDBCConnection.disconnection();
+		}
+		return cList;
+	}
+
+	@Override
+	public boolean addComputer(Computer c) {
+		String s = "Naissance)";
+		query = "INSERT INTO computer (name, introduced, discontinued, company_id)"
+				+ " VALUES (?,?,?,?)";
+		try {
+			preparedStatement = JDBCConnection.connection().prepareStatement(query);
+			preparedStatement.setString(1, c.getName());
+			preparedStatement.setDate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+
+	@Override
+	public boolean deleteComputer(Computer c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateComputer(Computer c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
