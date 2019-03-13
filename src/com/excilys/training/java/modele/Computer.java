7375a8a8407bd3109bf9bfa69f80255ae02c0ea5
Abdoul Mohamed <amohamed@excilys.com>
2019-03-13 14:09:07 +0100

    test date

--- src/com/excilys/training/java/modele/Computer.java
+++ src/com/excilys/training/java/modele/Computer.java
@@ -1,6 +1,7 @@
 package com.excilys.training.java.modele;
 
 import java.sql.Date;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class Computer {
@@ -52,7 +53,7 @@ public class Computer {
 	}
 
 	public String formatReadDate(Date date) {
-		SimpleDateFormat readingFormat = new SimpleDateFormat("dd-MMM-yy");
+		SimpleDateFormat readingFormat = new SimpleDateFormat("dd-MM-yyyy");
 		String d = readingFormat.format(date);
 		return d;
 	}
@@ -62,4 +63,40 @@ public class Computer {
 		return "Computer [id=" + id + ", name=" + name + ", introduced=" + formatReadDate(introduced) + ", discontinued="
 				+ formatReadDate(discontinued) + ", manufacturer_id=" + manufacturer_id + "]";
 	}
+	
+	public static void main(String[] args) {
+		String myDate = "01-04-1994";
+		SimpleDateFormat databaseFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+		try {
+			Date d2 = (Date) databaseFormat.parse(myDate);
+			System.out.println(d2.getTime());
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+//	{
+//		
+//		
+////		SimpleDateFormat inputFormat = new SimpleDateFormat("dd-MM-yyyy");
+////		Date d = new Date(1994, 04, 01);
+////
+////		String s2 = inputFormat.format(d);
+////		
+////		System.out.println(s2);
+//		
+////		SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yy");
+////	    String s = formatter.format(new Date(60725973600000L));
+////	    String myDate = "01-04-1994";
+////	    try {
+////			Date d2 = (Date) formatter.parse(myDate);
+////		    System.out.println(d2);
+////		} catch (ParseException e) {
+////			// TODO Auto-generated catch block
+////			e.printStackTrace();
+////		}
+////	    System.out.println(d.getTime());
+//	}
 }
