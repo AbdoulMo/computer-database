3da25b22b0f4c517a116adc8ed2a2203b42f4eaf
Abdoul Mohamed <amohamed@excilys.com>
2019-03-14 11:41:31 +0100

    implementation of all feature done

--- src/com/excilys/training/java/ui/Cli.java
+++ src/com/excilys/training/java/ui/Cli.java
@@ -15,27 +15,23 @@ import com.excilys.training.java.services.jdbc.JDBCCompany;
 import com.excilys.training.java.services.jdbc.JDBCComputer;
 
 public class Cli {
-	
+
 	private IDAOComputer computerDAO = new JDBCComputer();
 	private IDAOCompany companyDAO = new JDBCCompany();
 	private Scanner input = new Scanner(System.in);
 	private boolean leaveApp = false;
-			
+
 	public void displayMenu() {
-		System.out.println("Tapez un chiffre pour choisir l'action :\n"
-				+ "1: Lister les ordinateurs.\n"
-				+ "2: Lister les entreprises.\n"
-				+ "3: Afficher les détails d'un ordinateur.\n"
-				+ "4: Créer un ordinateur.\n"
-				+ "5: Mettre à jour les informations d'un ordinateur.\n"
-				+ "6: Supprimer un ordinateur.\n"
-				+ "7: Quitter.");
+		System.out.println("Tapez un chiffre pour choisir l'action :\n" + "1: Lister les ordinateurs.\n"
+				+ "2: Lister les entreprises.\n" + "3: Afficher les détails d'un ordinateur.\n"
+				+ "4: Créer un ordinateur.\n" + "5: Mettre à jour les informations d'un ordinateur.\n"
+				+ "6: Supprimer un ordinateur.\n" + "7: Quitter.");
 	}
-	
+
 	public String getInput() {
 		return input.nextLine();
 	}
-	
+
 	public boolean isLeaveApp() {
 		return leaveApp;
 	}
@@ -53,13 +49,13 @@ public class Cli {
 		switch (choice) {
 		case 1:
 			ArrayList<Computer> computerList = getComputerList();
-			for(Computer pc : computerList) {
+			for (Computer pc : computerList) {
 				System.out.println(pc);
 			}
 			break;
 		case 2:
 			ArrayList<Company> companyList = getCompanyList();
-			for(Company c : companyList) {
+			for (Company c : companyList) {
 				System.out.println(c);
 			}
 			break;
@@ -70,48 +66,96 @@ public class Cli {
 			System.out.println(computer);
 			break;
 		case 4:
-			System.out.println("Donnez le nom du nouveau Pc :\n");
-			//Computer c = new Computer();
+			Computer c = new Computer();
+			System.out.println("Le nom du nouveau Pc :\n");
+			c.setName(getInput());
+			System.out.println("La date d'introduction au format (yyyy-mm-dd)");
+			try {
+				java.util.Date utilDate = new SimpleDateFormat("yyyy-mm-dd").parse(getInput());
+				Date date = new Date(utilDate.getTime());
+				c.setIntroduced(date);
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("La date d'arret de production au format (yyyy-mm-dd)");
+			try {
+				java.util.Date utilDate = new SimpleDateFormat("yyyy-mm-dd").parse(getInput());
+				Date date = new Date(utilDate.getTime());
+				c.setDiscontinued(date);
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("L'identifiant de l'entreprise");
+			c.setManufacturer_id(Integer.parseInt(getInput()));
+			computerDAO.addComputer(c);
+			System.out.println("Nouvel ordinateur crée");
 			break;
 		case 5:
 			System.out.println("Pour continuer il faut connaitre l'identifiant de l'ordinateur...\n"
 					+ "Souhaitez vous continuer (o/n)");
 			reponse = getInput();
-			if(reponse.equals("o")) {
+			if (reponse.equals("o")) {
 				System.out.println("Entrez l'identifiant de l'ordinateur à modifier: \n");
 				id = Integer.parseInt(getInput());
 				computer = getComputerDetails(id);
 				boolean leaveOption = false;
 				String optionInput = new String();
-				while (leaveOption) {
-					System.out.println("Choisisez le paramètre a modifier "
-							+ "(1: Nom de l'ordinateur\n"
-							+ " 2: Date d'introduction\n"
-							+ " 3: Date d'arrete de production\n"
-							+ " 4: L'identifiant du fabriquant\n"
-							+ " 5: Pour terminer");
+				while (!leaveOption) {
+					System.out.println("Choisisez le paramètre a modifier " + "(1: Nom de l'ordinateur\n"
+							+ " 2: Date d'introduction\n" + " 3: Date d'arrete de production\n"
+							+ " 4: L'identifiant du fabriquant\n" + " 5: Pour terminer");
 					int choixModif = Integer.parseInt(getInput());
-					switch(choixModif){
-						case 1:
-							System.out.println("Entrez le nouveau nom: ");
-							optionInput = getInput();
-							computer.setName(optionInput);
-							System.out.println("Nom modifié !");
-							break;
-						case 2:
-							System.out.println("Entrez la nouvelle date (yyyy-mm-dd): ");
-							optionInput = getInput();
+					switch (choixModif) {
+					case 1:
+						System.out.println("Entrez le nouveau nom: ");
+						optionInput = getInput();
+						computer.setName(optionInput);
+						computerDAO.updateComputer(computer);
+						System.out.println("Nom modifié !");
+						break;
+					case 2:
+						System.out.println("Entrez la nouvelle date (yyyy-mm-dd): ");
 						try {
-							java.util.Date d = format.parse(optionInput);
+							java.util.Date utilDate = new SimpleDateFormat("yyyy-mm-dd").parse(getInput());
+							Date date = new Date(utilDate.getTime());
+							computer.setIntroduced(date);
 						} catch (ParseException e) {
 							// TODO Auto-generated catch block
 							e.printStackTrace();
 						}
-//							computer.setIntroduced(new D);
-							System.out.println("Nom modifié !");
-							break;
-						default:
-							break;
+						computerDAO.updateComputer(computer);
+						System.out.println("Date d'introduction modifié !");
+						break;
+					case 3:
+						System.out.println("Entrez la nouvelle date (yyyy-mm-dd): ");
+						optionInput = getInput();
+						try {
+							java.util.Date utilDate = new SimpleDateFormat("yyyy-mm-dd").parse(optionInput);
+							Date date = new Date(utilDate.getTime());
+							computer.setDiscontinued(date);
+						} catch (ParseException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						computerDAO.updateComputer(computer);
+						System.out.println("Date d'arrêt de production modifié !");
+						break;
+					case 4:
+						System.out.println("Entrez le nouvel identifiant: ");
+						optionInput = getInput();
+						computer.setManufacturer_id(Integer.parseInt(optionInput));
+						computerDAO.updateComputer(computer);
+						System.out.println("Identifiant fabriquant modifié !");
+						break;
+					case 5:
+						leaveOption = true;
+						System.out.println("Fin de la mise à jour");
+						break;
+					default:
+						System.out.println("Choix invalide veuillez réessayer !");
+						break;
 					}
 				}
 			}
@@ -120,13 +164,13 @@ public class Cli {
 			System.out.println("Pour continuer il faut connaitre l'identifiant de l'ordinateur...\n"
 					+ "Souhaitez vous continuer (o/n)");
 			reponse = getInput();
-			if(reponse.equals("o")) {
+			if (reponse.equals("o")) {
 				System.out.println("Entrez l'identifiant de l'ordinateur à supprimer: \n");
 				id = Integer.parseInt(getInput());
 				computer = getComputerDetails(id);
 				deleteComputer(id);
-				System.out.println("L'ordinateur "+ computer.getName() + 
-						" avec l'identifiant "+ computer.getId() +" a été supprimé.");
+				System.out.println("L'ordinateur " + computer.getName() + " avec l'identifiant " + computer.getId()
+						+ " a été supprimé.");
 			}
 			break;
 		case 7:
@@ -138,26 +182,26 @@ public class Cli {
 			break;
 		}
 	}
-	
+
 	public ArrayList<Computer> getComputerList() {
 		ArrayList<Computer> computerList = (ArrayList<Computer>) computerDAO.getAllComputers();
 		return computerList;
 	}
-	
-	public ArrayList<Company> getCompanyList(){
+
+	public ArrayList<Company> getCompanyList() {
 		ArrayList<Company> companyList = (ArrayList<Company>) companyDAO.getAllCompany();
 		return companyList;
 	}
-	
+
 	public Computer getComputerDetails(int id) {
 		Computer computer = computerDAO.getComputer(id);
 		return computer;
 	}
-	
+
 	public int createComputer(Computer c) {
 		return computerDAO.addComputer(c);
 	}
-	
+
 	public int updateComputer(int id) {
 		Computer c = computerDAO.getComputer(id);
 		return computerDAO.deleteComputer(c);
@@ -167,11 +211,11 @@ public class Cli {
 		Computer c = computerDAO.getComputer(id);
 		return computerDAO.deleteComputer(c);
 	}
-	
+
 	public static void main(String[] args) {
 		int choice = 0;
 		Cli app = new Cli();
-		while(!app.isLeaveApp()) {
+		while (!app.isLeaveApp()) {
 			app.displayMenu();
 			choice = Integer.parseInt(app.getInput());
 			app.action(choice);
