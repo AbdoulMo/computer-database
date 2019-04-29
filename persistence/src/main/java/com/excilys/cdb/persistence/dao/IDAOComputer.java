e4c1630129fc2529865f73bc54125caca1fb66dc
Abdoul Mohamed <amohamed@excilys.com>
2019-04-29 10:39:03 +0200

    core and persistence module

---
+++ persistence/src/main/java/com/excilys/cdb/persistence/dao/IDAOComputer.java
@@ -0,0 +1,19 @@
+package com.excilys.cdb.persistence.dao;
+
+import java.util.ArrayList;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.excilys.cdb.model.Computer;
+
+public interface IDAOComputer extends PagingAndSortingRepository<Computer, Integer> {
+
+	final String QUERY_DELETE_COMPUTER = "DELETE FROM computer WHERE company_id = :manufacturerId";
+
+	ArrayList<Computer> findAll();
+
+	@Query(value=QUERY_DELETE_COMPUTER, nativeQuery = true)
+	void deleteByManufacturerId(@Param("manufacturerId") Integer manufacturerId);
+}
